fdfb925bd2cd275e40e71fcb2455355efad181e0
unknown <mtbutt@deakin.edu.au>
2020-08-13 17:00:47 +1000

    With array

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var app = express();
 
-//app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/test', function(req,res){
+    console.log(req.query.array);
+    res.send(200);
+});
 
 var port = 3000;
 app.listen(port);
